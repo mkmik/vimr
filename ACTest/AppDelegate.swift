2d4e251c1278af1db18191eb22df2ea9e3b7c7cf
Tae Won Ha <h@taewon.de>
2017-11-17 16:04:28 +0100

    Add a broken target

---
+++ ACTest/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  ACTest
+//
+//  Created by hat on 17.11.17.
+//  Copyright © 2017 Tae Won Ha. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class Ap pDelegate: NSObject, NSApplicationDelegate {
+
+  @IBOutlet weak var window: NSWindow!
+
+
+  func applicationDidFinishLaunching(_ aNotification: Notification) {
+    // Insert code here to initialize your application
+  }
+
+  func applicationWillTerminate(_ aNotification: Notification) {
+    // Insert code here to tear down your application
+  }
+
+
+}
+
